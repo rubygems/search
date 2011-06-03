b819bea778bcf95fc03ff73e7abcb409aab175c4
Nick Zadrozny <nick@beyondthepath.com>
2011-06-03 12:23:49 -0500

    I am pedantic.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SearchRubygemsOrg::Application.config.secret_token = '41413f981a7a861a402eb0e05cb0c12a519fd4fb0a71bb479f906574b11a6e7b0d6910ae480870dea9be20d877e67342b5445736ec224c1a43d68533a95113f1'
+SearchRubygems::Application.config.secret_token = '41413f981a7a861a402eb0e05cb0c12a519fd4fb0a71bb479f906574b11a6e7b0d6910ae480870dea9be20d877e67342b5445736ec224c1a43d68533a95113f1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SearchRubygemsOrg::Application.routes.draw do
+SearchRubygems::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
