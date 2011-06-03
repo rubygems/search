6e230647bd2121fad294230bbd9174dbc63a2f04
Nick Zadrozny <nick@beyondthepath.com>
2011-06-03 10:27:30 -0500

    Initial creation.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SearchRubygemsOrg::Application.config.session_store :cookie_store, :key => '_search.rubygems.org_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SearchRubygemsOrg::Application.config.session_store :active_record_store
