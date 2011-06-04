da6d0af88f04534335c32f0d37d5544457ab8c22
Nick Zadrozny <nick@beyondthepath.com>
2011-06-03 23:11:33 -0500

    Starting on a super basic search controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SearchRubygems::Application.routes.draw do
+
+  root :to => 'search#search'
+  get '/search' => 'search#search'
   
   post '/webhook/gem' => 'webhook#gem'
   post '/webhook/gem/yank' => 'webhook#yank'
